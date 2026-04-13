2bf2b989efb539d24be0066a362a229b4c7dded7
Rhianna Burford <rhiannaburford@hotmail.co.uk>
2026-04-13 14:21:42 +0100

    Use a method reference :: without brackets ()

---
+++ Exercise2.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+@FunctionalInterface
+interface CheckEmpty {
+    boolean checkEmpty(String s);
+}
+
+
+public class Exercise2 {
+    /* Write a Java program to implement a lambda expression to check if a 
+    given string is empty */
+    public static void main(String[] args){
+        Scanner sinput = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String input = sinput.nextLine();
+        // Using a method reference is more efficient
+        CheckEmpty isEmpty = String::isEmpty;
+        System.out.println(isEmpty.checkEmpty(input) 
+        ? "The string is empty" 
+        : "The string is not empty");
+        sinput.close();
+    }
+}
