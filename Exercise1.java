a5ca57281f50d10dc190035e4ef4344010729437
Rhianna Burford <rhiannaburford@hotmail.co.uk>
2026-04-13 14:08:59 +0100

    Just adding some comments

--- Exercise1.java
+++ Exercise1.java
@@ -1,3 +1,4 @@
+// The interface must have exactly one abstract method
 @FunctionalInterface
 interface Sum {
     int add(int a, int b);
@@ -5,7 +6,9 @@ interface Sum {
 
 public class Exercise1 {
     public static void main(String[] args) {
+        // This is the lambda line
         Sum s = (a, b) -> a + b;
+        // Call the lambda implementation of add
         System.out.println("Sum = " + s.add(5, 7));
     }
 }
